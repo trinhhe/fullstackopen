0a168ba0c32b061d399b6a5a5e7a2c83e1f88734
henry <henry.trinh94@gmail.com>
2026-01-10 10:50:11 +0100

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -66,8 +66,17 @@ app.get('/api/persons/:id', (request, response) => {
     // } else {
     //     response.status(404).end()
     // }
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+    Person.findById(request.params.id)
+      .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
 })
 
